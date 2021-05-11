acf931217d7e3f58fe2f01db9afa6322e343c419
kosidocs <kelvinonaru@gmail.com>
2021-05-11 16:13:16 +0400

    feat: add rout to the router index file

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,23 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/details/:id",
+    name: "Details",
+    props: true,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "detail" */ "../views/Details.vue"),
+  },
+  {
+    path: "/create-post",
+    name: "CreatePost",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "detail" */ "../views/CreatePost.vue"),
   },
 ];
 
