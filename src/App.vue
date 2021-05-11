b13fc26aa14a4de2d20031c1d1f7935d1a998d7e
kosidocs <kelvinonaru@gmail.com>
2021-05-11 16:11:21 +0400

    feat: add navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <app-navigation></app-navigation>
+
+    <v-main transition="slide-x-transition">
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
+
+<script>
+import AppNavigation from "@/components/AppNavigation";
+
+export default {
+  name: "App",
+  components: {
+    AppNavigation,
+  },
+}
+</script>
 
 <style lang="scss">
 #app {
